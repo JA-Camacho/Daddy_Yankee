d5c9610a0cf9ac7838e6757650cc86259c559cdd
Jose Camacho <jcamachor1@est.ups.edu.ec>
2023-01-07 23:29:02 -0500

    Add javascript function to buttons

--- src/app/components/music/music.component.ts
+++ src/app/components/music/music.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./music.component.css']
 })
 export class MusicComponent {
+  audio1 = new Audio("../../../assets/Gasolina.mp3");
+  audio2 = new Audio("../../../assets/Despacito.mp3");
+  audio3 = new Audio("../../../assets/Con Calma.mp3");
+  sonar(id:number){
+    if (id === 1){
+      this.audio1.play();
+    }
+    if (id === 2){
+      this.audio2.play();
+    }
+    if(id === 3){
+      this.audio3.play();
+    }
+  }
+
+  pausar(id:number){
+    if (id === 1){
+      this.audio1.pause();
+    }
+    if (id === 2){
+      this.audio2.pause();
+    }
+    if(id === 3){
+      this.audio3.pause();
+    }
+  }
+
+  recargar(id:number){
+    if (id === 1){
+      this.audio1.load();
+    }
+    if (id === 2){
+      this.audio2.load();
+    }
+    if(id === 3){
+      this.audio3.load();
+    }
+  }
 
 }
